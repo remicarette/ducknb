d81fcc54ef1524a7af8125b7880899dd0990cbe7
remi carette <carette.remi@gmail.com>
2019-08-19 19:14:22 +0200

    add associations has_many & through for all tables

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :duck
+  has_many :reviews
 end
--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,3 +1,6 @@
 class Duck < ApplicationRecord
   belongs_to :user
+  has_many :duck_photos
+  has_many :bookings
+  has_many :reviews, through: :bookings
 end
