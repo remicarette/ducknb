cd9cd4084ec55a4412b5450bf5c2a4348f81818a
Simon Chretien <simon.chretien62@gmail.com>
2019-08-19 19:03:35 +0200

    add average methode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def home
+    average
+  end
+
+  private
+
+  def average
+    @best_ducks = []
+    ducks = Duck.all
+    ducks.each do |duck|
+      average = 0
+      duck.reviews.each do |review|
+        average += review.stars
+      end
+      average /= reviews.length
+      @bets_ducks.push(duck) if average == 5
+    end
   end
 end
