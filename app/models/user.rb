4581914bd4c2012368eb48c841f12a716e11cf58
remi carette <carette.remi@gmail.com>
2019-08-20 11:18:22 +0200

    add association user has_many duck-photos through ducks

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :ducks, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
+  has_many :duck_photos, through: :ducks
 end
