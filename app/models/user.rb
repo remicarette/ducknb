1d1d2c7d6a1393a45001574cf7ae9261d39b371d
remi carette <carette.remi@gmail.com>
2019-08-20 09:16:23 +0200

    add depend-destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :ducks 
-  has_many :bookings 
+  has_many :ducks, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 end
