5c665dc60d89051a01a167b0bf3b1a58d4568237
remi carette <carette.remi@gmail.com>
2019-08-20 13:40:34 +0200

    user photo is not required

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,4 @@ class User < ApplicationRecord
   validates :address, presence: true
   validates :zip_code, presence: true
   validates :city, presence: true
-  validates :photo, presence: true
 end
