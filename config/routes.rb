d8f499886b8a337c8ef094eb5e05419540be4434
remi carette <carette.remi@gmail.com>
2019-08-22 11:39:58 +0200

    solved bug in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :ducks do
-    resources :bookings, only [:create]
+    resources :bookings, only: :create
   end
-  resources :bookings, only [:index, :destroy] do
-    resources :reviews, only [:new, :create]
+  resources :bookings, only: [:index, :destroy] do
+    resources :reviews, only: [:new, :create]
   end
-  resources :reviews, only [:edit, :update, :destroy]
-  
-  # manage ducks 
+  resources :reviews, only: [:edit, :update, :destroy]
+
+  # manage ducks
   get 'profiles/:id/', to: "profiles#show", as: 'profile'
   get 'profiles/:id/new', to: "profiles#new", as: 'profile_duck_new'
 
 end
+
