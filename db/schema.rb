695a08d3aac8f7021c11db04784b41cd97a23f7f
remi carette <carette.remi@gmail.com>
2019-08-22 13:40:18 +0200

    price add on index, show, edit & new. Crashtest for each actions works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_124331) do
+ActiveRecord::Schema.define(version: 2019_08_22_095347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_08_21_124331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "price"
     t.index ["user_id"], name: "index_ducks_on_user_id"
   end
 
