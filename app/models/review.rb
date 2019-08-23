609263be953845aa6de603cd01a3bf3524039109
Moritz Michalak <moritzmichalak@gmx.de>
2019-08-23 14:53:40 +0200

    reviews done

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,12 +5,12 @@ class Review < ApplicationRecord
   validates :stars, presence: true
   validates :booking, presence: true
 
-def convert_date(date_at)
+  def convert_date(date_at)
     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     datum = date_at.to_s
     year = datum[0..3]
     month = months[datum[5..6].to_i - 1]
     day = datum[8..9]
-    return "#{month} #{day}, #{year}"
+    return "#{day} #{month}, #{year}"
   end
 end
