20d9d706868ae867e013be7727d1038b3ab026c0
remi carette <carette.remi@gmail.com>
2019-08-29 14:31:02 +0200

    resolve problem for navigation

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :ducks do
     resources :bookings, only: :create
   end
-  resources :bookings, only: [:index, :destroy] do
+  resources :bookings, only: [:index, :show, :destroy] do
     resources :reviews, only: [:new, :create]
   end
   resources :reviews, only: [:edit, :update, :destroy]
